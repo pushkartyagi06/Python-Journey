4232ee973794450344904827b9dffc3361fe3d37
pushkar <pushkartyagi006@gmail.com>
2026-04-02 23:04:16 +0530

    Add files via upload

---
+++ problem 2.py
@@ -0,0 +1,9 @@
+marks = []
+
+for i in range(6):
+    m = int(input("Enter marks: "))
+    marks.append(m)
+
+marks.sort()
+
+print("Sorted marks:", marks)
---
+++ problem 3.py
@@ -0,0 +1,9 @@
+numbers = []
+
+for i in range(4):
+    num = int(input("Enter number: "))
+    numbers.append(num)
+
+total = sum(numbers)
+
+print("Sum is:", total)
